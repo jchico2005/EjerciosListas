02465f03328f3ae7c7865f1b7a6a0803ba9c85ed
DESARROLLO1 <DESARROLLO1@DESARROLLO1>
2025-05-09 21:30:16 -0500

    correcion de codigo en clase padre

--- Models/Padre.cs
+++ Models/Padre.cs
@@ -1,13 +1,10 @@
-﻿using System.Globalization;
+﻿namespace EjerciosListas.Models;
 
-namespace EjerciosListas.Models;
-
-public class Padre: Persona
+public class Padre : Persona
 {
     public string DireccionTreabajo { get; set; } = string.Empty;
     public int Telefono { get; set; }
     public string Cargo { get; set; } = string.Empty;
     public string ColorOjos { get; set; } = string.Empty;
-
     public List<string> DireccioTrabajo { get; set; } = new();
 }
